dbee02e78655cde21ac9b1b143bc455a6a073dd6
boredcoder411 <taamarcos@gmail.com>
2023-06-12 18:34:58 +0200

    added keyboard support (somewhat)

--- kernel.c
+++ kernel.c
@@ -1,18 +1,25 @@
+#include <sys/io.h>
 #define WHITE_TXT 0x07
 
 void k_clear_screen();
 unsigned int k_printf(char *message, unsigned int line);
 extern void disable_cursor();
+void k_keyboard();
 
-void k_main() {
+void k_main()
+{
 	k_clear_screen();
 	k_printf("Hello, world! Welcome to my kernel.", 0);
+	k_keyboard();
+	k_clear_screen();
 };
 
-void k_clear_screen(){
-	char *vidmem = (char *) 0xb8000;
+void k_clear_screen()
+{
+	char *vidmem = (char *)0xb8000;
 	unsigned int i = 0;
-	while(i < (80 * 25 * 2)) {
+	while (i < (80 * 25 * 2))
+	{
 		vidmem[i] = ' ';
 		i++;
 		vidmem[i] = WHITE_TXT;
@@ -20,19 +27,24 @@ void k_clear_screen(){
 	};
 };
 
-unsigned int k_printf(char *message, unsigned int line) {
-	char *vidmem = (char *) 0xb8000;
+unsigned int k_printf(char *message, unsigned int line)
+{
+	char *vidmem = (char *)0xb8000;
 	unsigned int i = 0;
 
 	i = (line * 80 * 2);
 
-	while(*message != 0) {
-		if(*message == '\n') {
+	while (*message != 0)
+	{
+		if (*message == '\n')
+		{
 			line++;
 			i = (line * 80 * 2);
 			*message++;
-		} else {
-			vidmem[i] =* message;
+		}
+		else
+		{
+			vidmem[i] = *message;
 			*message++;
 			i++;
 			vidmem[i] = WHITE_TXT;
@@ -40,5 +52,24 @@ unsigned int k_printf(char *message, unsigned int line) {
 		};
 	};
 
-	return(1);
+	return (1);
+}
+
+void k_keyboard()
+{
+	char *c = 0;
+	do
+	{
+
+		if (inb(0x60) != c) // PORT FROM WHICH WE READ
+		{
+			c = inb(0x60);
+			if (c > 0)
+			{
+
+				k_printf(c, 2); // print on screen
+			}
+		}
+
+	} while (c != 1); // 1= ESCAPE
 }
