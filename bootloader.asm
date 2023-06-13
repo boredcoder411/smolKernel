3655570b415cf0b78d13b2049eee35767b6d2090
boredcoder411 <105937655+boredcoder411@users.noreply.github.com>
2023-06-13 16:33:04 +0200

    Update bootloader.asm

--- bootloader.asm
+++ bootloader.asm
@@ -6,29 +6,9 @@ section .text
 	dd - (0x1BADB002 + 0x00)
 
 global start
-global disable_cursor
 extern k_main
-
-disable_cursor:
-	pushf
-	push eax
-	push edx
- 
-	mov dx, 0x3D4
-	mov al, 0xA
-	out dx, al
- 
-	inc dx
-	mov al, 0x20
-	out dx, al
- 
-	pop edx
-	pop eax
-	popf
-	ret
 
 start:
 	cli
 	call k_main
-	call disable_cursor
 	hlt
